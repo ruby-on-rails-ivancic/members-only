960cad3b2fcf6fdabdd93f8848202ced981f2958
Liam Ivancic <ivancic.liam@gmail.com>
2025-04-09 12:51:03 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Members-Only
+
+This was a lesson on user authentication with Devise. Users can sign up to create posts. Only signed-in users can create posts and see the authors of all other posts. a non-logged-in user will not be able to create posts. They can view all posts, but cannot see the authors of those posts until they log in.
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create ]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "Post created successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.expect(post: [ :title, :body ])
+  end
+end
